a07e7023c87d5c5c9076592f1d1c577f3e061caf
Nurhad <m.nurhadbarokah@gmail.com>
2021-11-29 21:13:38 +0700

    add mudblazor

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace blazor_wasm
 {
@@ -18,6 +19,8 @@ namespace blazor_wasm
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
